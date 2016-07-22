fc1b18bb44b9dbd7ab76d3a454df04e8990c140a
Matthew <mcitron@cern.ch>
2016-07-22 14:01:10 +0100

    updated ssh heppc script

--- sshHeppc.sh
+++ sshHeppc.sh
@@ -1,9 +1,9 @@
-computerList=(heppc238 heppc52) 
-#heppc108 - crap
+computerList=(heppc238 heppc52 heppc108)
 #heppc164 - slc5
 minUsers=99
 for computer in ${computerList[@]}; 
 do 
+    echo $computer
     users=$(ssh $computer 'users')
     arr=($users)
     numUsers=$(for i in ${arr[@]}; do echo $i;done | sort -n | uniq | wc -l)
